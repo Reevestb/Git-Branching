4a29b5d13f68736a80f6f175e4ae476cba41bf29
Dorkydubs <rsgacre@gmail.com>
2024-06-04 11:15:40 +0100

    update to Js

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 let likeEggs = window.prompt("You better like eggs bro.");
+const mainTitle = document.querySelector("h1");
+let subTitle = document.querySelector("h3");
+let lowerImage = document.querySelector("img");
 if (likeEggs !== "yes") {
-  document.style.Color = "red";
+  mainTitle.style.color = "red";
+  subTitle.textContent = `${subTitle.textContent}, not that you care`;
+  lowerImage.src =
+    "https://cdn.pixabay.com/photo/2015/02/18/05/32/angry-640314_1280.jpg";
+} else {
+  mainTitle.style.color = "red";
 }
